8dedbf31176bbb1800e31f5de0be590353bbf917
FabriDamazio <fabridamazio@gmail.com>
2025-07-09 13:08:23 -0300

    fly deploy configs

--- src/config/config.exs
+++ src/config/config.exs
@@ -7,9 +7,9 @@
 # General application configuration
 import Config
 
-config :site,
-  ecto_repos: [Site.Repo],
-  generators: [timestamp_type: :utc_datetime]
+# config :site,
+#   ecto_repos: [Site.Repo],
+#   generators: [timestamp_type: :utc_datetime]
 
 # Configures the endpoint
 config :site, SiteWeb.Endpoint,
--- src/lib/site/repo.ex
+++ src/lib/site/repo.ex
@@ -1,5 +1,5 @@
 defmodule Site.Repo do
-  use Ecto.Repo,
-    otp_app: :site,
-    adapter: Ecto.Adapters.Postgres
+  #  use Ecto.Repo,
+  #    otp_app: :site,
+  #    adapter: Ecto.Adapters.Postgres
 end
